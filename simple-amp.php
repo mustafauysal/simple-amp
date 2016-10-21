938dfbdb4396071b03783376c5699d6d506ce7b5
mustafauysal <m_uysl@hotmail.com>
2016-10-21 17:24:50 +0300

    scaffolding

---
+++ simple-amp.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Simple Amp
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     simple-amp
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Simple_Amp
+ */
